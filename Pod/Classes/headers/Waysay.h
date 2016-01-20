3e22ae5e9ea258ec863f42e413ed67df267204f0
comptonmc <matt@waysay.com>
2016-01-19 21:14:22 -0500

    Updated header and library

--- Pod/Classes/headers/Waysay.h
+++ Pod/Classes/headers/Waysay.h
@@ -7,11 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "WSLocationManager.h"
 
 @interface Waysay : NSObject
 
-+(void) initWithApplicationID:(NSString *)applicationID andOrganizationID:(NSString *)organizationID;
-+(void) authenticateUser:(NSString *)username andPassword:(NSString *)password;
+@property (nonatomic, readonly) BOOL downloadingJWT;
+@property (nonatomic, readonly) BOOL downloadingSettings;
+@property (strong, nonatomic) WSLocationManager *wsLocationManager;
+
++ (Waysay *) sharedApplication;
+
+-(id) initWithApplicationID:(NSString *)applicationID andOrganizationID:(NSString *)organizationID;
 +(void) launchInboxFromViewController:(id)viewController;
++(void) registerDeviceID:(NSData *)deviceID;
++(void) resetUserWithCompletionHandler:(void (^)(NSDictionary *result, NSError *error))completionBlock;
 
 @end
