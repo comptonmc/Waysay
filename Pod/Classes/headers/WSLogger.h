19083dde523a4e7984ee38d6b569af19d13913a5
comptonmc <matt@waysay.com>
2016-02-15 16:22:08 -0500

    Env variable fixes 0.6.10

---
+++ Pod/Classes/headers/WSLogger.h
@@ -0,0 +1,30 @@
+//
+//  WSLogger.h
+//  ws-ios-sdk-dev2
+//
+//  Created by Matt Compton on 2/9/16.
+//  Copyright © 2016 Waysay. All rights reserved.
+//
+
+#ifndef WSLogger_h
+#define WSLogger_h
+
+
+#ifdef DEBUG
+    #define WSLog( logString, ... ) NSLog((@"DEBUG: %s:[Line: %d] - " logString), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+    #define WSLog(...)
+#endif
+
+
+#define WSError( logString, ... ) NSLog((@"ERROR: %s:[Line: %d] - " logString), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+#ifdef WSDEVELOPMENT
+#define WSENV @"development"
+#elif WSSTAGING
+#define WSENV @"staging"
+#else
+#define WSENV @"production"
+#endif
+
+#endif /* WSLogger_h */
---
+++ Pod/Classes/headers/WSNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  WSNavigationController.h
+//  WS_SDK_Dev
+//
+//  Created by Matt Compton on 8/8/15.
+//  Copyright (c) 2015 Waysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSNavigationController : UINavigationController
+
+@end
