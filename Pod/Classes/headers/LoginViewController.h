db9067aa3da7284fb8c34d81b646af57616b64b4
comptonmc <matt@waysay.com>
2016-02-19 13:14:49 -0500

    Customize login message and skip

---
+++ Pod/Classes/headers/LoginViewController.h
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.h
+//  ws-ios-sdk-dev2
+//
+//  Created by Matt Compton on 12/7/15.
+//  Copyright © 2015 Waysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol WSLoginViewControllerDelegate;
+
+@interface LoginViewController : UIViewController
+
+@property (nonatomic, weak) id<WSLoginViewControllerDelegate> delegate;
+
+@property (strong, nonatomic) NSString *titleMessage;
+@property (strong, nonatomic) NSString *initialTitleMessage;
+@property (strong, nonatomic) NSString *initialSkipMessage;
+
+@end
+
+@protocol WSLoginViewControllerDelegate <NSObject>
+
+- (void)refreshPreferences;
+
+@end
